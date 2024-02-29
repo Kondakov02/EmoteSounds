85ef56e372f59a248c7f0d5a61a2670e5a58a58d
Дмитрий Кондаков <kond.dmi@gmail.com>
2024-02-29 23:52:39 +0300

    Create controller class, carry over the variables

---
+++ EmoteLaugh/Patches/EmoteController.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace EmoteLaugh.Patches
+{
+    internal class EmoteController : NetworkBehaviour
+    {
+        private static float oldAudioSourceVolume;
+
+        private static AudioClip oldAudioClip;
+
+        private const string emoteParamInAnimator = "emoteNumber";
+
+        private static int previousEmoteID = 0;
+
+        private static bool playingInterruptableAudio = false;
+
+
+    }
+}
